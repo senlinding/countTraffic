ef4fbaa87cc589e48ddb1a5380e90089afce2d21
linzi <1209090446@qq.com>
2017-04-17 19:01:12 +0800

    log

--- log.py
+++ log.py
@@ -3,9 +3,16 @@ from time import *
 
 logFileName = ""
 
-def getTimeStr():
+class timeMode:
+    (showAll, notYear) = range(1, 3)
+def getTimeStr(mode=timeMode.showAll):
     lt = localtime()
-    timeStr = "%04d%02d%02d %02d:%02d:%02d" % (lt.tm_year, lt.tm_mon, lt.tm_mday, lt.tm_hour, lt.tm_min, lt.tm_sec)
+
+    if mode == timeMode.notYear:
+        timeStr = "%02d%02d %02d:%02d:%02d" % (lt.tm_mon, lt.tm_mday, lt.tm_hour, lt.tm_min, lt.tm_sec)
+    else:
+        timeStr = "%04d%02d%02d %02d:%02d:%02d" % (lt.tm_year, lt.tm_mon, lt.tm_mday, lt.tm_hour, lt.tm_min, lt.tm_sec)
+
     return timeStr
 
 def initLogFile():
@@ -14,20 +21,19 @@ def initLogFile():
         logFileName = (getTimeStr() + ".log").replace(' ', '_').replace(":", "")
 
     with open(logFileName, "w+") as fr:
-        fr.write('log start time: ' + str(ctime(time())) + "\n")
+        fr.write('log start time: ' + getTimeStr() + "\n")
 
     print "init log file[%s] ok!" % (logFileName)
 
-def writeLog(buf, fileName=""):
+def writeLog(buf, fileName="", mode=timeMode.notYear):
     global logFileName
     if fileName != "":
         logFileName = fileName
 
-    log = getTimeStr() + ": " + buf
+    log = getTimeStr(mode) + ": " + buf
     #print log
     with open(logFileName, "a") as fr:
         fr.write(log + "\n")
-
 
 if __name__ == '__main__':
     initLogFile()
